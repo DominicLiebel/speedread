1ba8e8ed1f52b6e05a631aed8113ee181150670b
Dominic Liebel <dominic.liebel@gmail.com>
2022-11-14 18:04:16 +0100

    Added layout.html

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
+
+books = [
+    {
+        'author': 'Daniel Kahnemann',
+        'title': 'Vermessung der Welt',
+        'words': 5000,
+        'content': "Die Vermessung der Welt...."
+    },
+    {
+        'author': 'Author',
+        'title': 'So good they can\'t ignore you',
+        'words': 2000,
+        'content': 'Be so good, that they can\'t ignore you!'
+    }
+]
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Speedread</h1>'
+    return render_template('index.html', books=books)
 
-app.run(debug=True, host='68.183.79.24', port=80)
+
+app.run(debug=True, host='127.0.0.1', port=5000)
